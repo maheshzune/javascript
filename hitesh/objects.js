0b4365a27eb03717f945afd988c3d2c752e3a832
maheshzune <maheshzune03@gmail.com>
2024-05-03 12:16:11 +0530

    objrcts2 addded

--- hitesh/objects.js
+++ hitesh/objects.js
@@ -7,15 +7,15 @@ const hello={
     class:'fybbaca',
     dob:'3-11-2004'
 }
-console.log(hello.name);
-console.log(hello["name"]);
-console.log( hello[mysymbol]);
-console.log(hello['surname']);
+// console.log(hello.name);
+// console.log(hello["name"]);
+// console.log( hello[mysymbol]);
+// console.log(hello['surname']);
 
 hello.class='sybbaca';
 // Object.freeze(hello);
 hello.class='sybaca';
-console.log(hello);
+// console.log(hello);
 
 hello.greeting= function (params) {
   console.log('hello everyone');    
@@ -24,5 +24,17 @@ hello.greetingtwo= function (params) {
   console.log(`hello everyone ,${this.name}`);    
 }
 
-console.log(hello.greeting());
-console.log(hello.greetingtwo());
+// conslog(hello.greetingtwo());
+
+//***************************object 2 ++++++++++++++++++++++++++++ */
+
+const regularuser = {
+     username:{
+      userfullname:{
+        fullname:'mahesh',
+        surnam:'zune'
+      }
+     }
+}
+
+console.log(regularuser.username.userfullname.surnam)
